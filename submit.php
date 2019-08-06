362f16b1f1b78e270784fcaae738ba3a76a9edd9
Jacob Abbou <jacobabbou@outlook.com>
2019-08-06 19:12:48 -0400

    Add Questions Form

---
+++ submit.php
@@ -0,0 +1,22 @@
+<?php
+
+// var_dump($_POST);
+
+if($_POST["submit"]) {
+
+$recipient = "jacobabbou@outlook.com";
+
+$name = $_POST["senderName"];
+$email = $_POST["senderEmail"];
+
+$subject = "Denture Clinic Website Question";
+$message = $_POST["message"];
+
+$contentForEmail= "Name: $sender\nEmail: $senderEmail\n\n$message";
+
+mail($recipient, $subject, $contentForEmail, "From: $sender <$senderEmail>");
+
+// $thankYou = "<p> Thank you! Your message has been sent. </p>";
+}
+
+?>
