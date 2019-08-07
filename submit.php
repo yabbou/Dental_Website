d4b7f58d7d6700d6529aa351e59f9dc4782d6cb1
Jacob Abbou <jacobabbou@outlook.com>
2019-08-07 18:55:43 -0400

    Fixed message button (max 50 messages)

--- submit.php
+++ submit.php
@@ -1,22 +1,16 @@
 <?php
 
-// var_dump($_POST);
-
 if($_POST["submit"]) {
 
-$recipient = "jacobabbou@outlook.com";
+    $recipient = "jacobabbou@outlook.com";
+    $name = $_POST["senderName"];
+    $email = $_POST["senderEmail"];
 
-$name = $_POST["senderName"];
-$email = $_POST["senderEmail"];
+    $subject = "Denture Clinic Website Question";
+    $message = $_POST["message"];
+    $contentForEmail= "Name: $sender\nEmail: $senderEmail\n\n$message";
 
-$subject = "Denture Clinic Website Question";
-$message = $_POST["message"];
-
-$contentForEmail= "Name: $sender\nEmail: $senderEmail\n\n$message";
-
-mail($recipient, $subject, $contentForEmail, "From: $sender <$senderEmail>");
-
-// $thankYou = "<p> Thank you! Your message has been sent. </p>";
+    mail($recipient, $subject, $contentForEmail, "From: $sender <$senderEmail>");
 }
 
 ?>
